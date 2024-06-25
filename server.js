ff730aca7a9b24fbb11a467aef18a058a5b9cd5c
psai12 <sairampallapothula@gmail.com>
2024-06-25 23:56:15 +0530

    addlogo

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv').config({path:'../config.env'});
 const ejs=require('ejs');
-
+const path = require('path');
 const app=express();
 
 app.set('ejs','views');
 
-app.use(express.static('css'));
-
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
      res.render('index.ejs');
