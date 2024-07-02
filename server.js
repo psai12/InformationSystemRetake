926ba53bd2e521f31f92985c7921e6e2b2655d2e
psai12 <sairampallapothula@gmail.com>
2024-07-02 22:54:30 +0530

    database

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose=require('mongoose');
 const dotenv=require('dotenv').config({path:'../config.env'});
 const path = require('path');
 const app=express();
+const database=require('mongoose');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
