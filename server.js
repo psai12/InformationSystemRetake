b8016ed029690d7c558c1a7cdf6e5cbfcabb309e
psai12 <sairampallapothula@gmail.com>
2024-07-06 17:59:18 +0530

    loginpageaded

--- server.js
+++ server.js
@@ -14,4 +14,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
      res.render('index.ejs');
 });
+app.get('/login',(req,res)=>{
+     res.render('login.ejs');
+});
 app.listen("2000",()=>{console.log('Server started!')});
