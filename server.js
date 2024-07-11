0c473f415c4d4aebd1f05dc4cfcd810553c04d0b
20007384 <puratibrahmateja@gmail.com>
2024-07-11 15:15:19 +0530

    resgiterrouteforerror

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
 app.get('/',(req,res)=>{
      res.render('index.ejs');
 });
@@ -23,8 +22,44 @@ app.get('/login',(req,res)=>{
 });
 
 app.get('/register',(req,res)=>{
-     const {email,password,confirmpassword}=req.body;
+     res.render('register.ejs');
+})
+.post('/register',async (req,res,next)=>{
+  
+     CheckForDetails(req,res,next);
+     
+     try
+     {
+        const user
+     }
+     catch(exp)
+     {
 
-     console.log(email,password,confirmpassword)
+     }
 });
+
+function CheckForDetails(req,res,next){
+     const {name,email,password,confirmpassword}=req.body;
+     if(name=='')
+     {
+          console.log('name is empty');
+          return res.status(400).send('Name is required');
+     }
+     else if(email=='')
+     {
+          console.log('email is empty');
+          return res.status(400).send('Email is required');
+     }
+     else if(password=='')
+     {
+          console.log('password is empty');
+          return res.status(400).send('Password is required');
+     }
+     else if(confirmpassword=='')
+     {
+          console.log('confirmpassword is empty');
+          return res.status(400).send('Confirm password is required');
+     }
+     next();
+}
 app.listen("2000",()=>{console.log('Server started!')});
