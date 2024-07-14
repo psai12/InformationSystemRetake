99b9f785220fd0ce82028550ffb6de1199710528
20007384 <puratibrahmateja@gmail.com>
2024-07-15 02:22:24 +0530

    addimages

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,20 @@
 function ImageSelector(id)
 {
-    const imagesection=document.getElementById(id).firstElementChild;
-    const innerHtml=imagesection.innerHTML;
 
-    console.log(imagesection);
+    const imagesection=document.getElementById(id);
+    const image=imagesection.firstElementChild.src;
+    const button=imagesection.lastElementChild;
+    const overlay=document.querySelector('.overlaypreview');
+    let overlayimage=overlay.lastElementChild.src;
+    button.addEventListener('click',()=>{
+        overlay.style.display="flex";
+          overlay.lastElementChild.src=image;
+    })
+   
 }
+
+const cross=document.querySelector('.cross');
+const overlay=document.querySelector('.overlaypreview');
+cross.addEventListener('click',()=>{
+  overlay.style.display="none";
+});
