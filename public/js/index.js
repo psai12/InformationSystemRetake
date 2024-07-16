6ab2362c5d54b23e61cfca50f0fcdd0bcc1ed88c
20007384 <puratibrahmateja@gmail.com>
2024-07-17 02:14:58 +0530

    cartthing

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ function ImageSelector(id,value=false)
 
     const imagesection=document.getElementById(id);
     const image=imagesection.firstElementChild.src;
+    console.log(image)
     const button=imagesection.lastElementChild;
     const overlay=document.querySelector('.overlaypreview');
     let overlayimage=overlay.lastElementChild.src;
@@ -11,7 +12,7 @@ function ImageSelector(id,value=false)
     {
         button.addEventListener('click',()=>{
             overlay.style.display="flex";
-              overlay.lastElementChild.src=image;
+              overlay.querySelectorAll('img')[1].src=image;
               overlay.querySelector('h3').innerText=price;
         })
     }
@@ -26,7 +27,13 @@ cross.addEventListener('click',()=>{
 });
 
 
+let cart=[]
 function AddToCart(element)
 {
+    const img=element.querySelectorAll('img')[1];
+    const h3=element.querySelector('h3');
+
+    const obj={img,h3};
+
 
 }
